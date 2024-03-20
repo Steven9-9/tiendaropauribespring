c64ece3bb7b2ab1b7064c28c0d9f596cebc8f330
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-03-20 12:08:07 -0500

    tiendabd

--- src/main/java/com/example/Store/modelos/Producto.java
+++ src/main/java/com/example/Store/modelos/Producto.java
@@ -8,11 +8,33 @@ public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name="nombreProducto",nullable = false,length = 20)
     private String nombreProducto;
+    @Column(name="referencia",nullable = false,length = 15)
     private String referencia;
+    @Column(name="talla",nullable = false,length = 6)
     private String talla;
+    @Column(name="cantidadBodega",nullable = false)
     private Integer cantidadBodega;
+    @Column(name="precioUnitario",nullable = false)
     private Integer precioUnitario;
+    @Column(name="descripcion",nullable = false,length = 30)
     private String descripcion;
+    @Column(name="fotografia",nullable = false,length = 250)
     private String fotografia;
+
+    public Producto() {
+    }
+
+
+    public Producto(Integer id, String nombreProducto, String referencia, String talla, Integer cantidadBodega, Integer precioUnitario, String descripcion, String fotografia) {
+        this.id = id;
+        this.nombreProducto = nombreProducto;
+        this.referencia = referencia;
+        this.talla = talla;
+        this.cantidadBodega = cantidadBodega;
+        this.precioUnitario = precioUnitario;
+        this.descripcion = descripcion;
+        this.fotografia = fotografia;
+    }
 }
